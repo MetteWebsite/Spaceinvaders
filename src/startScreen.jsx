6cb92dc9880ddf1f28960b2feb9401d1ca5989da
Rebecca Arkhed Stationär <rebar310@student.liu.se>
2024-11-13 15:13:02 +0100

    Förstasidan börjar fåå rätt utseende

--- src/startScreen.jsx
+++ src/startScreen.jsx
@@ -4,15 +4,18 @@ import React from "react";
 function StartScreen() {
   return (
     <div style={backgroundStyle}>
-      <h1>Welcome to the Start Screen</h1>
-      <p>This is where you can start your journey!</p>
-      {/* Add other content here as needed */}
+      <div style={textContainerStyle}>
+        <h1 style={titleStyle}>Course Slayer</h1>
+        <h2 style={subtitleStyle}>stay sharp, slay smart</h2>
+
+        {/* Add other content here as needed */}
+      </div>
     </div>
   );
 }
 
 // Sennare som sista grej så kan vi flytta så att css ffår en egen fil.
-// Define the style for the background
+// Denna stylar endast bakgrundbilden
 const backgroundStyle = {
   backgroundImage: `url(${"src/assets/startScreen.PNG"})`,
   backgroundSize: "cover", // This makes the image cover the entire area
@@ -23,6 +26,28 @@ const backgroundStyle = {
   alignItems: "center", // Optional: Center content vertically
   justifyContent: "center", // Optional: Center content horizontally
   color: "white", // Optional: Text color for contrast
+};
+
+// Denna styling omringar all text på första skärmen
+const textContainerStyle = {
+  //backgroundColor: "rgba(0, 0, 0, 0.5)", // denna kan man använda sig av för att veta vart textcontainer området är
+  padding: "20px",
+  borderRadius: "8px",
+  textAlign: "center",
+};
+
+const titleStyle = {
+  color: "blue",
+  fontFamily: "Impact", //leta efter bättre font
+  //fontStyle: "bold",
+  textTransform: "uppercase", // Optional: Transforms text to uppercase
+  fontSize: "90px", // Sets the font size
+};
+
+const subtitleStyle = {
+  color: "black",
+  fontFamily: "Courier New",
+  fontSize: "40px", // Sets the font size
 };
 
 export default StartScreen;
